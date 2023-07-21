d15353869854dd4b54215538bbaa9f1617604b3d
Mateusz-Michalec <mateusz.michalec.poczta@gmail.com>
2023-07-21 21:11:17 +0200

    homepage almost done
    font changed,
    added bootstrap imports,
    added some global classes

---
+++ src/constants/images.ts
@@ -0,0 +1,11 @@
+import hero_engineer from "../assets/images/hero_engineer.svg";
+import home_speakers from "../assets/images/home_speakers.svg";
+import home_ehxibitors from "../assets/images/home_exhibitors.svg";
+import hala_rcs from "../assets/images/hala_rcs-min.jpg";
+
+export default {
+  hero_engineer,
+  home_speakers,
+  home_ehxibitors,
+  hala_rcs,
+};
