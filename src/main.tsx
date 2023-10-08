7b16ae92ffdb67c45547495209cd89aa4a2e806f
Mateusz Michalec <mateusz.michalec.pl@gmail.com>
2023-10-08 09:38:20 +0200

    routes changed

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/engineer-day">
+  <BrowserRouter basename="/radomski-dzien-inzyniera">
     <App />
   </BrowserRouter>
 );
